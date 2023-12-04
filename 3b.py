fb250a171d4748e4099887feaad28143ef006c0d
mjutn <mathijnbecker@live.nl>
2023-12-04 16:31:21 +0100

    Do day 3 challenge 2

--- 3b.py
+++ 3b.py
@@ -2,20 +2,28 @@ import re
 
 file = open('3_input')
 lines = []
+stars = {}
 total = 0
+
 for line in file:
     lines.append('.'+ line.strip('\n') + '.')
 width = len(lines[0])
 edge = '.' * width
 lines.insert(0, edge)
 lines.append(edge)
-nonSymbols = ['0','1','2','3','4','5','6','7','8','9','.']
+nonSymbols = ['0','1', '2','3','4','5','6', '7', '8','9','.']
 for i in range(1,len(lines)-1):
     for m in re.finditer('\d+', lines[i]):
         start, end = m.span()
         number = m.group()
         for j in range(i-1, i+2):
             for k in range(start-1, end+1):
-                if lines[j][k] not in nonSymbols:
-                    total += int(number)
+                if lines[j][k] == '*':
+                    if (j,k) not in stars:
+                        stars[(j,k)] = [int(number)]
+                    else:
+                        stars[(j,k)].append(int(number))
+for key in stars:
+    if len(stars[key])==2:
+        total += stars[key][0]*stars[key][1]
 print(total)
