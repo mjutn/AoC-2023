b6d6c010bc278507f32dfb920fd29472b51109c0
mjutn <mathijnbecker@live.nl>
2023-12-05 20:09:16 +0100

    Do day 5 challenge 2

--- 5b.py
+++ 5b.py
@@ -16,9 +16,9 @@ class Encoding:
                     self.src[key].append(ints[1])
                     self.rng[key].append(ints[2])
                     break
-                if self.src[key][index] <= ints[1]:
+                if self.src[key][index] >= ints[1]:
                     continue
-                elif self.src[key][index] > ints[1]:
+                elif self.src[key][index] < ints[1]:
                     self.dest[key].insert(index, ints[0])
                     self.src[key].insert(index, ints[1])
                     self.rng[key].insert(index, ints[2])
@@ -42,35 +42,27 @@ inputRanges = sections[0].split()[1:]
 inputRanges = list(map(int, inputRanges))
 best = 2147483647
 for i in range(10):
-    print(i)
     source = inputRanges[2*i]
     fullRange = inputRanges[2*i+1]
-    seed = source
-    while seed < source + fullRange:
+    seed = source + fullRange - 1
+    while seed >= source:
         loc = seed
-        skip = source+fullRange-loc
+        skip = loc-source
         for key in keys:
             for index in range(len(enc.src[key])):
                 src = enc.src[key][index]
                 rng = enc.rng[key][index]-1
                 dest = enc.dest[key][index]
                 if loc > src + rng:
-                    if index == len(enc.src[key])-1:
-                        break
-                    continue    
-                elif src <= loc <= src + rng:
+                    skip = min(skip, loc-(src+rng))
+                    break
+                if src <= loc <= src + rng:
+                    skip = min(skip, loc-src)
                     loc = dest-src+loc
-                    skip = min(skip, src + rng - loc)
-                    break
-                elif loc < src:
-                    skip = min(skip, src-loc)
-                    break
-                else:
-                    print('something is going wrong')
-            
+                    break 
         best = min(best, loc)
         if skip > 0:
-            seed = seed + skip
+            seed = seed - skip
         else:
-            seed = seed + 1
+            seed = seed - 1
 print(best)
