fbf6f8b27dccae84b9a6b559580c273e26645f63
mjutn <mathijnbecker@live.nl>
2023-12-06 09:44:36 +0100

    Clean up input day 6b

--- 6b.py
+++ 6b.py
@@ -1,16 +1,12 @@
 with open('6_input') as file:
     categories = file.read().split('\n')
-times = categories[0].split()[1:]
 time = ''
-distances = categories[1].split()[1:]
+time = time.join(categories[0].split()[1:])
 distance = ''
-for index in range(len(times)):
-    time = time + times[index]
-    distance = distance + distances[index]
+distance = distance.join(categories[1].split()[1:])
 timeInt = int(time)    
 distanceInt = int(distance)
 total = 1
-
 for i in range(timeInt):
     if i * (timeInt-i) > distanceInt:
         total = total * (timeInt-2*i+1)
