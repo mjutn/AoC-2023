39930b7df3263bf0aad0bd284151663324df54b6
mjutn <mathijnbecker@live.nl>
2024-09-26 15:46:44 +0200

    Almost do day 12, result too high

--- 12.py
+++ 12.py
@@ -1,22 +1,29 @@
 import re
 
-with open('12_test') as file:
+def checkGroup(input, groups):
+    result = 0
+    if groups == []:
+        return 1
+    size = groups[0]
+    pattern = '[\.\?]'+size*"[\#\?]"+"[\.\?]"
+    i = 0
+    while i<len(input):
+        match = re.search(pattern, input[i:])
+        if match == None:
+            break
+        begin, end = match.span()
+        result += checkGroup(input[(i+end-1):], groups[1:])
+        i = i + begin + 1
+    return result
+
+with open('12_input') as file:
     lines = file.read().split('\n')
 
 total = 0
 
 for line in lines:
     input, groups = line.split()
-    print(input +"|"+ groups+"\n")
-    groups = [int(x) for x in groups if x.isdigit()]
-    hashtagsTotal = sum(groups)
-    minLength = hashtagsTotal + len(groups) -1
-    dotsAdded = len(input)-minLength
-
-    if dotsAdded == 0:
-        total += 1
-        continue
-    
-    unknownGroups = re.split('\.', input)
-    unknownGroups = list(filter(None, unknownGroups))
-    print(unknownGroups)
+    groups = [int(x) for x in groups.split(',')]
+    input = '.'+input+'.'
+    total += checkGroup(input, groups)
+print(total)
