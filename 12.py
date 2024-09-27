9bdcdcadd8c414f6892fd44b79cb0a97e9456996
mjutn <mathijnbecker@live.nl>
2024-09-27 12:28:29 +0200

    Do another attempt at day 12
    Not taking #s further on the line into account, which leads to a lot of invalid solution being counted

--- 12.py
+++ 12.py
@@ -8,15 +8,20 @@ def checkGroup(input, groups):
     pattern = '[\.\?]'+size*"[\#\?]"+"[\.\?]"
     i = 0
     while i<len(input):
+        # print(f"Level: {len(groups)} total: {result} size: {size} input: {input} position: {i}")
         match = re.search(pattern, input[i:])
         if match == None:
-            break
+            return result
         begin, end = match.span()
-        result += checkGroup(input[(i+end-1):], groups[1:])
+        if "#" in input[begin:end] and len(groups) == 1:
+            return result + 1
+        nextInput = input[(i+end-1):]
+        nextGroups = groups[1:]
+        result += checkGroup(nextInput, nextGroups)
         i = i + begin + 1
     return result
 
-with open('12_input') as file:
+with open('12_test_b') as file:
     lines = file.read().split('\n')
 
 total = 0
@@ -26,4 +31,5 @@ for line in lines:
     groups = [int(x) for x in groups.split(',')]
     input = '.'+input+'.'
     total += checkGroup(input, groups)
+    print(total)
 print(total)
